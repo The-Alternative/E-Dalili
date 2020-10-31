128ec80a228c282e1038ccca92ffe3258f470df2
Al-Badael <71507872+Al-Badael@users.noreply.github.com>
2020-10-31 02:42:46 +0200

    Add files via upload

---
+++ database/migrations/2020_10_18_111644_create_countries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('iso_code');
+            $table->string('currency');
+            $table->boolean('is_active');
+            $table->string('call_perfix');
+            $table->string('zip_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
